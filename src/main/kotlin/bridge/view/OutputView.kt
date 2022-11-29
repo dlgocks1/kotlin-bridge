322b7da10a41c0b7e8f15365732ebb186815b712
dlgocks1 <cksgodl1@naver.com>
2022-11-29 14:00:29 +0900

    style : BridgeResult를 아규먼트로 받게 변경

--- src/main/kotlin/bridge/view/OutputView.kt
+++ src/main/kotlin/bridge/view/OutputView.kt
@@ -1,5 +1,6 @@
 package bridge.view
 
+import bridge.model.BridgeResult
 import bridge.model.BridgeStatus
 import bridge.utils.*
 
@@ -10,8 +11,8 @@ class OutputView {
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      */
-    fun printMap(bridgeMap: String) {
-        println(bridgeMap)
+    fun printMap(bridgeResult: BridgeResult) {
+        println(bridgeResult.toString())
     }
 
     /**
